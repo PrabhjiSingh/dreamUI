e981d1011d797713241a119105b02f44672c1961
prabhjisingh <prabhji.singh26@gmail.com>
2022-08-07 19:20:10 +0530

    aewrsdtgy

---
+++ dreamcatcher/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+
+const bg_image = new URL("./images/home_bg.jpg", import.meta.url)
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+}
+
+export default App;
---
+++ dreamcatcher/src/Components/HomeComponent/Home.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+export default class Home extends PureComponent {
+    render() {
+        return (
+
+            <div className='home-container'>
+                <h1>Header 1</h1>
+            </div>
+
+        )
+    }
+}
